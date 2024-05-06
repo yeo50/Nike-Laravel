678a3f000d2a55616b9abdb6865268d449a8abf0
yeo50 <minhtet3294@gmail.com>
2024-05-06 20:05:33 +0630

    finished hero section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import {headerLogo} from '../assets/images';
 import {hamburger} from  '../assets/icons'
-
+import { navLinks } from '../constants';
 // run rafce 
 const Nav = () => {
   return (
@@ -9,7 +9,18 @@ const Nav = () => {
             <a href="/">
                 <img src={headerLogo} alt="logo" width={130} height={29}/>
             </a>
-            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:' ></ul>
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden' >
+               {navLinks.map((item)=> (
+                <li key={item.label}>
+                    <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
+                        {item.label}
+                    </a>
+                </li>
+               ))}
+            </ul>
+            <div className='hidden max-lg:block'>
+                <img src={hamburger} alt="hamburger" width={25} height={25}/>
+            </div>
         </nav>
     </header>
   )
