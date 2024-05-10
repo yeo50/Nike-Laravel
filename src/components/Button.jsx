753b66a717f7e2df079ab0a47154200a8b6a0364
yeo50 <minhtet3294@gmail.com>
2024-05-10 16:01:48 +0630

    end with subscribe and footer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 
-const Button = ({label, iconUrl, backgroundColor, textColor, borderColor}) => {
+const Button = ({label, iconUrl, backgroundColor, textColor, borderColor, fullWidth}) => {
   return (
     <button className={`flex justify-center items-center 
     gap-2 px-7 py-4 border font-montserrat text-lg leading-none
     ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}`: "bg-coral-red  text-white border-coral-red"}
   
-     rounded-full`}>{label}
+     rounded-full ${fullWidth && 'w-full'}`}>{label}
    
    {iconUrl && <img src={iconUrl} alt="arrow right icon" className="w-5 h-5 ml-1 " />}
     </button>
