8eb770c213eca8c1baca1d0bd8c6ffcb8eef211b
yeo50 <minhtet3294@gmail.com>
2024-05-10 12:07:17 +0630

    special offer & customer review section added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,13 @@
 
-const Button = ({label, iconUrl}) => {
+const Button = ({label, iconUrl, backgroundColor, textColor, borderColor}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">{label}
+    <button className={`flex justify-center items-center 
+    gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+    ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}`: "bg-coral-red  text-white border-coral-red"}
+  
+     rounded-full`}>{label}
    
-    <img src={iconUrl} alt="arrow right icon" className="w-5 h-5 ml-1 " />
+   {iconUrl && <img src={iconUrl} alt="arrow right icon" className="w-5 h-5 ml-1 " />}
     </button>
   )
 }
